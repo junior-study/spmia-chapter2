ca9f66a76c856151751657a2d61ee02e080ca89f
jayden-lee <gwanggi.lee@gmail.com>
2021-05-02 13:17:07 +0900

    Update License logic

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
@@ -2,45 +2,37 @@ package com.thoughtmechanix.licenses.controllers;
 
 import com.thoughtmechanix.licenses.model.License;
 import com.thoughtmechanix.licenses.services.LicenseService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value="v1/organizations/{organizationId}/licenses")
+@RequestMapping(value = "v1/organizations/{organizationId}/licenses")
+@RequiredArgsConstructor
 public class LicenseServiceController {
-    @Autowired
-    private LicenseService licenseService;
 
-    @RequestMapping(value="/{licenseId}",method = RequestMethod.GET)
-    public License getLicenses( @PathVariable("organizationId") String organizationId,
-                                @PathVariable("licenseId") String licenseId) {
+    private final LicenseService licenseService;
 
-        //return licenseService.getLicense(licenseId);
-        return new License()
-            .withId(licenseId)
-            .withOrganizationId(organizationId)
-            .withProductName("Teleco")
-            .withLicenseType("Seat");
+    @GetMapping("/{licenseId}")
+    public License getLicenses(@PathVariable("organizationId") String organizationId,
+                               @PathVariable("licenseId") String licenseId) {
+
+        return licenseService.getLicense(organizationId, licenseId);
     }
 
-    @RequestMapping(value="{licenseId}",method = RequestMethod.PUT)
-    public String updateLicenses( @PathVariable("licenseId") String licenseId) {
+    @PutMapping("/{licenseId}")
+    public String updateLicenses(@PathVariable("licenseId") String licenseId) {
         return String.format("This is the put");
     }
 
-    @RequestMapping(value="{licenseId}",method = RequestMethod.POST)
-    public String saveLicenses( @PathVariable("licenseId") String licenseId) {
+    @PostMapping("/{licenseId}")
+    public String saveLicenses(@PathVariable("licenseId") String licenseId) {
         return String.format("This is the post");
     }
 
-    @RequestMapping(value="{licenseId}",method = RequestMethod.DELETE)
+    @DeleteMapping("/{licenseId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public String deleteLicenses( @PathVariable("licenseId") String licenseId) {
+    public String deleteLicenses(@PathVariable("licenseId") String licenseId) {
         return String.format("This is the Delete");
     }
 }
--- licensing-service/src/main/java/com/thoughtmechanix/licenses/services/LicenseService.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/services/LicenseService.java
@@ -8,23 +8,24 @@ import java.util.UUID;
 @Service
 public class LicenseService {
 
-    public License getLicense(String licenseId){
-        return new License()
-                .withId(licenseId)
-                .withOrganizationId( UUID.randomUUID().toString() )
-                .withProductName("Test Product Name")
-                .withLicenseType("PerSeat");
+    public License getLicense(String organizationId, String licenseId) {
+        return License.builder()
+            .id(licenseId)
+            .organizationId(UUID.randomUUID().toString())
+            .productName("Test Product Name")
+            .licenseType("PerSeat")
+            .build();
     }
 
-    public void saveLicense(License license){
-
-    }
-
-    public void updateLicense(License license){
+    public void saveLicense(License license) {
 
     }
 
-    public void deleteLicense(License license){
+    public void updateLicense(License license) {
+
+    }
+
+    public void deleteLicense(License license) {
 
     }
 
